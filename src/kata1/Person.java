4dca08d1414d233cd50e02d8ba78d3f96635a22d
Usuario <Usuario@10.22.148.103>
2019-09-18 14:22:59 +0100

    Project updated

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -5,10 +5,46 @@
  */
 package kata1;
 
+import java.util.Date;
+
 /**
  *
  * @author Usuario
  */
 public class Person {
+    private String name;
+    private Date birthday;
+    private int age;
+    private final double SECONDS_TO_MILISECONDS = 1000;
+    private final double MINUTES_TO_SECONDS =60.0;
+    private final double HOURS_TO_MINUTES =60.0;
+    private final double DAYS_TO_HOURS = 24.0;
+    private final double YEARS_TO_DAYS = 365.25;
+
+    public Person(String name, Date birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public int getAge() {
+        return toYears(new Date().getTime()- birthday.getTime());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private int toYears(long l) {
+        return (MINUTES_TO_SECONDS*
+                HOURS_TO_MINUTES*
+                YEARS_TO_DAYS*
+                DAYS_TO_HOURS*
+                SECONDS_TO_MILISECONDS);
+    }
+
     
 }
